c7e4b324fa7c37421219fc6f21445c4c9cd3ad6e
psw35 <psw35@nau.edu>
2025-03-18 20:40:10 -0700

    New changes

--- web_interface/web-interface.py
+++ web_interface/web-interface.py
@@ -5,7 +5,7 @@ from datetime import datetime
 
 app = Flask(__name__)
 
-API_GATEWAY_URL = "http://api-gateway:5000/api/api_gateway"
+API_GATEWAY_URL = "http://localhost:3000"
 
 @app.route('/')
 def index():
